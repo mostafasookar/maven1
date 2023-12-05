e2544819ce7afa381f8a389bd3bf482032fb322d
Moustafa Mohamed Sokkar <mostafasookar@hotmail.com>
2023-12-05 12:33:03 +0100

    Add new

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,7 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
-		
+			
+		p = new Person();
+		p.setId("5");
+		p.setAge(33);
+		p.setFirstName("webhook");
+		p.setLastName("sokkar");
+		persons.add(p);	
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
